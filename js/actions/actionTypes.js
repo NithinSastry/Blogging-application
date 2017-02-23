40fc73a0f86543fc45272d05fcb43c5e73fd548a
nithin.sastry@outlook.com <nithinsastry.tellapuri@in.pega.com>
2017-02-23 22:38:09 +0530

    Defined action types.

---
+++ js/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const GET_POST = "GET_POST";
+export const MODIFY_POST = "MODIFY_POST";
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+export const CREATE_USER = "CREATE_USER";
+export const EDIT_USER = "EDIT_USER";
+export const SEARCH = "SEARCH";
+export const GET_SESSION_CONTEXT = "GET_SESSION_CONTEXT";
